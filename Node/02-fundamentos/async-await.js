11a2a6a7a413e9c759fd830650e9c1b04da800ca
CharlieRParker <57034134+CharlieRParker@users.noreply.github.com>
2019-11-19 00:27:07 +0100

    Add files via upload

--- Node/02-fundamentos/async-await.js
+++ Node/02-fundamentos/async-await.js
@@ -3,22 +3,36 @@
  */
 //poniendo la palabra async tenemos lo mismo que en la promesa comentada debajo
 
-let getNombre = async() => {
+// let getNombre = async() => {
 
+//     return 'Charlie'; //lo que regres aqui la funcion es = que un resolve
 
-    return 'Charlie'; //lo que regres aqui la funcion es = que un resolve
+// };
 
-};
+let getNombre = () => {
+    return new Promise((resolve, reject) => {
 
-// let getNombre = () => {
-//     return new Promise((resolve, reject) => {
-//         resolve('Fernando');
-//     });
-// }
+        setTimeout(() => {
 
-getNombre().then(nombre => {
-        console.log(nombre);
+            resolve('Marita');
+
+        }, 3000);
+
+    });
+}
+
+//await debe estar dentro de una funcion async, los async pueden estar solos
+
+let saludo = async() => {
+
+    let nombre = await getNombre();
+    // no se va a ejecutar ese funcion hasta tener la respuesta del await get nombre
+    return `Hola ${ nombre }`;
+}
+
+saludo().then(mensaje => {
+        console.log(mensaje);
     })
-    .catch(err => {
-        console.log('error de Async', err);
-    })
+    // .catch(err => {
+    //     console.log('error de Async', err);
+    // })
